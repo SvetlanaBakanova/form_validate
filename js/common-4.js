f8ee8c5490f3c9e407338efa9992f67dfd4ebcfb
SvetlanaBakanova <69769350+SvetlanaBakanova@users.noreply.github.com>
2021-02-24 07:02:23 +0200

    Ajax

--- js/common-4.js
+++ js/common-4.js
@@ -1,4 +1,127 @@
 
+// let phoneFields = document.querySelectorAll("input[type='tel']");
+// let im = new Inputmask("+380(99) 999 99 99");
+$popupId.find('input[name="phone"]').mask("+380(99)999-99-99");
+popupId.mask(phoneFields);
+
+$popupId.on('click', 'input[name="submit"]', function () {
+    $popupId.find('.js-form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2,
+                maxlength: 20
+            },
+            phone: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Поле обязательное для заполнения",
+                minlength: "Имя не должно содержать меньше 2 символов",
+                maxlength: "Имя не должно содержать больше 20 символов"
+            },
+            phone: {
+                required: "Поле обязательное для заполнения",
+            },
+            email: {
+                required: "Поле обязательное для заполнения",
+                email: "Введите корректный E-mail"
+            }
+        },
+        
+    })
+    })
+    
+
+
+// function validateForm ($popupId) {
+
+// 		var $popupId = $popupId;
+
+// 		$popupId.find('input[name="phone"]').mask("+380(99)999-99-99");
+
+// 		$popupId.on('focus', 'input[name="phone"]', function(){
+
+// 			 $popupId.find('input[name="phone"]').mask("+380(99)999-99-99");
+
+// 		});
+
+// 		$popupId.on('click', 'input[name="submit"]', function(){
+
+// 			$popupId.find('.js-form').validate({
+// 			    rules: {
+// 			        name: {
+// 			            required: true,
+// 			            minlength: 2,
+// 			            maxlength: 20
+// 			        },
+// 			        phone: {
+// 			            required: true
+// 			        },
+// 			        email: {
+// 			        	required: true,
+// 			        	email: true
+// 			        }
+// 			    },
+// 			    messages: {
+// 			        name: {
+// 			            required: "Поле обязательное для заполнения",
+// 			            minlength: "Имя не должно содержать меньше 2 символов",
+// 			            maxlength: "Имя не должно содержать больше 20 символов"
+// 			        },
+// 			        phone: {
+// 			            required: "Поле обязательное для заполнения",
+// 			        },
+// 			        email: {
+// 			        	required: "Поле обязательное для заполнения",
+// 			        	email: "Введите корректный E-mail"
+// 			        }
+// 			     },
+// 			    submitHandler: function() {
+// 					$popupId.find('.js-form').ajaxSubmit(function(){
+// 						// var $formContent = $popupId.find('.popup__inner').html(),
+// 							fd = new FormData();
+// 						if ($popupId.find('input[name="name"]').length > 0) {
+// 							fd.append('name', $popupId.find('input[name="name"]').val());
+// 						}
+						
+// 						if ($popupId.find('input[name="phone"]').length > 0) {
+// 							fd.append('phone', $popupId.find('input[name="phone"]').val());
+// 						}
+
+// 						if ($popupId.find('input[name="email"]').length > 0) {
+// 							fd.append('email', $popupId.find('input[name="email"]').val());
+// 						}
+
+// 						if ($popupId.find('.popup__title').length > 0) {
+// 							fd.append( 'popup__title', $popupId.find('.popup__title').text() );
+// 						}
+						
+// 						$.ajax({
+// 							type: "POST",
+// 							url: "mail.php",
+// 							data: fd
+// 							// success: function(data){
+// 							// 	// $popupId.find('.popup__inner').html(data);
+// 							// 	$popupId.find('.popup__close').on('click', function(){
+// 							// 		$popupId.find('.popup__inner').html($formContent);
+// 							// 	});
+// 							// },
+// 							beforeSend: function(){
+								
+//                             }
+// 		       			})
+// 					});
+// 				}
+// 			});
+// 		});
+// 	}
 let phoneFields = document.querySelectorAll("input[type='tel']");
 let im = new Inputmask("+380(99) 999 99 99");
 im.mask(phoneFields);
